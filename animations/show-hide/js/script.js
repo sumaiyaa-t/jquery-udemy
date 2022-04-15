0b3bfe04a0278487864aaa5d2e9c19e4c1e07e9f
stmeem <103250128+sumaiyaa-t@users.noreply.github.com>
2022-04-15 11:22:13 +0600

    Show and hide elements

---
+++ animations/show-hide/js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  //Hide elements
+  $(".blue-box").hide();
+
+  //Show elements
+  $(".blue-box").show();
+
+  //Toggle hide and show
+  $(".green-box").toggle();
+
+  //Show and hide as animations
+  $(".red-box").hide(1000);
+  $(".red-box").show(1000);
+
+});
